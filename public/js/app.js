dd8a14af213ee88ac5e5d5f701a060561c954f7f
MANOJ SHANKAR REDDY KARRI <manojshankarreddykarri@manojs-mbp.myfiosgateway.com>
2022-03-09 07:16:18 -0500

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+console.log('Client side javascript file is loaded')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // console.log('testing!')
+    
+    const location = search.value
+    
+    // console.log(location)
+
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent= ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    // fetch(`http://localhost:3000/weather?address=`+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+                messageOne.textContent = data.error
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const forecast = (longtitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=2d45a4680875b6da90ec3b15a29d6936&query=' + latitude + ',' + longtitude +'&units=f'
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('unable to connect to weather stack services!', undefined)
+        } else if (body.error) {
+            callback(body.error.info, undefined)
+        } else {
+            // callback(undefined, {
+            //     weatherDesription: response.body.current.weather_descriptions[0],
+            //     currentTemperature: response.body.current.temperature,
+            //     feelsLike: response.body.current.feelslike
+            // })
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+
+}
+
+module.exports = forecast
